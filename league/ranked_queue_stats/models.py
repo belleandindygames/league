8128723ad1970d8424b8bbf20e93571f707cd3ad
Matt Belley <mattbelley@gmail.com>
2018-02-08 22:37:52 -0700

    Updated models for User and Gamelist

--- league/ranked_queue_stats/models.py
+++ league/ranked_queue_stats/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    summoner_name = models.Charfield(max_length=30)
+    account_id = models.Charfield(max_length=30)
+    time_stamp = models.Charfield(max_length=30)
+
+
+class Gamelist(models.Model):
+    account_id = models.IntegerField()
+    platform_id = models.Charfield(max_length=30)
+    game_id = models.BigIntegerField()
+    champion = models.PositiveSmallIntegerField()
+    queue = models.Charfield(max_length=30)
+    season = models.SmallIntegerField()
+    time_stamp = models.Charfield(max_length=30)
+    role = models.Charfield(max_length=30)
+    lane = models.Charfield(max_length=30)
+    win = models.Charfield(max_length=30)
+
+
